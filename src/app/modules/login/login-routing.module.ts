bb00bec5df93dda5623bfe8cb641ad6fbaf0d4f9
Your Name <you@example.com>
2023-03-22 08:17:01 -0600

    mariscal

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -1,7 +1,6 @@
 import { LoginComponent } from './pages/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RegistroComponent } from './pages/registro/registro.component';
 
 
